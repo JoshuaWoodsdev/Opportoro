18d0a7f3d04c20c991d191a110453aaa54053f01
Joshua Woods <nordev@protonmail.com>
2025-02-18 23:35:50 -0600

    setting up of the database

---
+++ pocketbaseFiles/pb_migrations/1739941947_updated_users.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text4166911607",
+    "max": 0,
+    "min": 0,
+    "name": "username",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "json687618750",
+    "maxSize": 0,
+    "name": "profileJson",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "json"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("text4166911607")
+
+  // remove field
+  collection.fields.removeById("json687618750")
+
+  return app.save(collection)
+})
